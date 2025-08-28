e1efadcdb1109602316eff7b9db0a3db66fae3a3
GiwrgosGln <georgios.glinias@gmail.com>
2025-08-28 23:23:27 +0300

    feat: update .gitignore and configure Rollup visualizer for build stats

--- VitalSense.Web/vite.config.ts
+++ VitalSense.Web/vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteTsconfigPaths()],
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ open: true, filename: "stats.html" })],
+    },
+  },
 });
