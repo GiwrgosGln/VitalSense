5e65f05110cee596385f2ed3bac3dae487bf451b
GiwrgosGln <georgios.glinias@gmail.com>
2025-10-15 00:46:16 +0300

    feat: implement create questionnaire template component and update questionnaire templates list

--- VitalSense.Web/src/app/routes/app/questionnaires.tsx
+++ VitalSense.Web/src/app/routes/app/questionnaires.tsx
@@ -1,4 +1,3 @@
-import { CreateQuestionnaireTemplate } from "@/features/questionnaires/components/create-questionnaire-template";
 import { QuestionnaireTemplatesList } from "@/features/questionnaires/components/questionnaire-templates-list";
 
 const QuestionnairesRoute = () => {
